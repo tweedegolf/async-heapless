e112ffdd0334cf8d8969602f7d32e446b5b73d52
Lars Jellema <lars.jellema@gmail.com>
2020-12-11 15:15:17 +0100

    Handle feedback, cleanup

--- src/oneshot.rs
+++ src/oneshot.rs
@@ -1,46 +1,48 @@
-// Concurrency isn't easy and implementing the primitives is even harder. I found myself in need of
+// # Build your own async primitive
+
+// Concurrency isn't easy and implementing its primitives is even harder. I found myself in need of
 // some no-std, no-alloc Rust async concurrency primitives and decided to write some. I kept the
-// scope small so the code would be simple enough that even I could understand it. Even so, it
-// still involved futures, wakers, atomics, drop and unsafe. When you're done reading, you'll know
-// what those are for and even you can implement concurrency primitives!
+// scope small so even you and I can understand it. Even so, it still involved futures, wakers,
+// atomics, drop and unsafe. I'll introduce each of those to you while building a simple primitive.
+// At the end, even you will be able to implement your own primitives!
 
-// A oneshot channel is a synchronization primitive used to send a single message from one task to
-// another. When the sender has sent a message and it is ready to be received, the oneshot notifies
-// the receiver. Because this is an async primitive, that happens by the receiver awaiting and the
-// sender waking the receiver.
+// First, our primitive:
 
-//! A oneshot channel allowing sending of a single message with async, without depending on std or
-//! alloc.
+//! A oneshot channel is a synchronization primitive used to send a single message from one task to
+//! another. When the sender has sent a message and it is ready to be received, the oneshot
+//! notifies the receiver. The receiver is a Rust async `Future` that can be `await`ed. Our
+//! implementation does not depend on std or alloc.
 //!
 //! See https://tweedegolf.nl/blog/TODO/async-oneshot for a full description of the internals.
 
-// What do we mean with all that? First, without depending on std or alloc means we can't use
-// many common libraries and that is also why I needed to build my own primitives. It also means we
-// have to place all data somewhere on the stack.
+// What do we mean by all that? First, not depending on std or alloc means we can't use many
+// common libraries. Rust async is still a new area and the libraries so far depend on
+// allocating memory at least. That makes their primitives easier to build but also unusable on
+// embedded devices which can't afford an allocator. I work with such devices regularly and so I've
+// learned to avoid allocation whenever possible. We'll see that we can write our oneshot without
+// allocation just fine.
 
-// I also want to use async. Async is Rust's built-in tool for writing asynchronous functions that
-// look like synchronous code. Rust async leaves the choice of how tasks are run to the executor.
-// The executor runs the tasks it's given until they await a future. At that point, the executor
-// starts polling the future. It gives the future a waker and asks it if it's ready yet. If it is,
-// it returns a value from the await expression and that task can continue as normal.
+// Next, we don't want to block the entire CPU while waiting for the message to be sent. For one
+// thing, it won't be sent if the CPU is blocked. When an operating system is available, it can
+// schedule the sending task while the receiving task is blocked and there's no problem. Small
+// embedded devices usually can't afford an operating system though, so you're left with
+// implementing task switching manually. That can involve a lot of moving state from your only call
+// stack into a struct and back so you can use the call stack to continue another task.
 
-// If the future isn't ready, it stores the waker and tries to make progress. When it can't make
-// progress because it's waiting for some external event, it returns with `Poll::Pending`. Most
-// executors don't poll a future again until the waker they stored is used to wake it. At some
-// point, something might happen that makes the future able to make progress again. For example, an
-// interrupt might come in that tells the CPU it finished receiving some data from a peripheral.
-// From that point on, the future is able to make progress again and wants to be scheduled again by
-// the executor. In order to tell it that, the waker must be used to wake the task. That means that
-// interrupt handler needs to be able to access the waker. After making progress and waking a
-// number of times, the future will be ready and the task can continue.
+// Rust async can do it for you: First, you choose an async executor to run your tasks. These
+// executors can be much simpler than even an embedded OS. Then you write your tasks as normal.
+// When you reach a point where you need to wait for another task to finish something, you just
+// write `.await`. The executor will store the state of your call stack and continue with a task
+// that isn't blocked. When the thing you were waiting for is done, the executor will wake your
+// task up again.
 
-// Because this is a heapless abstraction, we need to store all data on the stack. We begin with a
-// `Oneshot` struct which will contain the data both the sender and receiver need to access in
-// order to transfer the message. Then we create a `Sender` and `Receiver` struct which contain a
-// reference to the `Oneshot`. We can then give the sender to one task and the receiver to another.
-// Since they both have a reference to the `Oneshot`, they must be shared references, which means
-// we can't safely modify just anything inside. Instead, we need to use types which are safe to
-// modify through shared references.
+// Let's begin with a `Oneshot` struct which will contain the data both the sender and receiver
+// need to access in order to transfer the message. It needs to be created high on the call stack
+// so it will outlive all tasks that might reference it. Then we create a `Sender` and `Receiver`
+// struct which contain a reference to the `Oneshot`. We can then give the sender to one task and
+// the receiver to another. Since they both have a reference to the `Oneshot`, they must be shared
+// references, which means we can't safely modify just anything inside. Instead, we need to use
+// types which are safe to modify through shared references.
 
 // ### The message
 
@@ -48,7 +50,8 @@
 // directly if we wait for both the sender and receiver to be ready for the transfer: We'd know
 // where the data is on the sender side and where it should go on the receiver side. We call this
 // synchronous communication, both the sender and the receiver need to be ready before the
-// communication happens. It has its uses, but in this case we're trying to build an asynchronous
+// communication happens. It has its uses, but in this case, we don't want the the sender to wait
+// for the receiver since it won't get a response back anyway.
 // oneshot, one which allows the sender to forget about the message and do other things long before
 // the receiver is ready to receive it. So instead, we'll store the message in the `Oneshot` where
 // it can wait for the receiver to be ready.
@@ -76,10 +79,12 @@
 // message afterwards. If the receiver happened to check in between, it would try to read a message
 // before there was ever stored one and it would end up with a bunch of garbage.
 
-// In order to make concurrent memory accesses safe, people invented atomics. We'll remember if we
+// In order to make concurrent memory accesses safe, atomics were invented. We'll remember if we
 // have a message with an `AtomicBool`. Atomics make sure no task sees a partial update of the
 // atomic. The atomic either updates completely or not at all. On top of that, it also helps us
-// synchronize access to other memory.
+// synchronize access to other memory: The value of the `AtomicBool` determines whether we allow
+// the sender or the receiver to access the message. That way, they never access it at the same
+// time.
 
 // Finally, we want to use Rust async/await to wait until the message is ready. This requires us to
 // store a waker. To be able to update the waker from a shared reference, we use the very nice
@@ -266,11 +271,13 @@ impl<'a, T> Sender<'a, T> {
 }
 
 // The receiver we get from `split` is a future that can be awaited for the message being sent.
-// This is the last tricky bit in our implementation. A `Future` is a thing with a `poll` function.
-// In it, it receives a pinned reference to the `Future` and a context containing a waker. It can
-// return one of two things: `Poll::Ready(message)` indicates the future is done and the `.await`
-// will return with the `message`. `Poll::Pending` means the future is not done yet and Rust will
-// handle control back to the executor that called `poll` so it can find another task to run.
+// This is the last tricky bit in our implementation. A `Future` is a thing with a `poll` function
+// which will be called by the executor. In it, it receives a pinned reference to itself and a
+// context containing a waker. In our case, we don't care that the reference is pinned. The `poll`
+// can return one of two things: `Poll::Ready(message)` indicates the future is done and the
+// `.await` will return with the `message`. `Poll::Pending` means the future is not done yet and
+// Rust will handle control back to the executor that called `poll` so it can find another task to
+// run.
 
 // The `Future` will be polled for the first time when it is first awaited. After that, in
 // principle it won't run again until we use the waker we received in the previous `poll` to wake
@@ -318,15 +325,16 @@ impl<'a, T> Drop for Receiver<'a, T> {
 // And that wraps up the implementation of our oneshot. Not too bad, right? Hopefully you have a
 // good idea now about what goes into a concurrency primitive. The code for this blog is available
 // at https://github.com/tweedegolf/async-heapless so please create an issue or pull request if
-// there's something wrong with it. Also see TODO where I use (the unsafe methods of) the oneshot
-// to build an async SPI driver.
+// there's something wrong with it. Also see
+// https://github.com/tweedegolf/async-spi/blob/main/src/common.rs where I use (the unsafe methods
+// of) the oneshot to build an async SPI driver.
 
 // I want to encourage you to try writing your own abstractions. For starters, you could create a
 // channel with a capacity of one message where the sender and receiver can be used multiple times
 // and the sender can block until there is space in the channel. After that, you could look into
-// channels that can store multiple messages or allow multiple concurrent senders or receivers. But
-// don't try to add everything at once, each of those things is hard enough on its own.
+// channels that can store multiple messages or allow multiple concurrent senders or receivers. Or
+// maybe you want build a primitive where both tasks wait for each other and then exchange
+// messages at the same time. Just make sure you don't try to add everything at once, each of those
+// things is hard enough on its own.
 
 // Finally, remember to document every unsafe function with the conditions needed to use it safely!
-
-// TODO: example usage
